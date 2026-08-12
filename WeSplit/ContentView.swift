a1b8937ff162d3342d191befb20db6283b7f3648
Yadnesh <yadneshdongre24@gmail.com>
2026-08-13 00:38:30 +0530

    [ FEATURE ] add number of people picker and navigation title

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -15,11 +15,20 @@ struct ContentView: View {
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Form {
-            Section {
-                TextField ("Amount", value: $billAmount, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
-                    .keyboardType(.decimalPad)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField ("Amount", value: $billAmount, format: .currency(code: Locale.current.currency?.identifier ?? "INR"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach (2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
             }
+            .navigationTitle("WeSplit")
         }
     }
 }
